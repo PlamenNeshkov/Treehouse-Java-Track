881f8e5e3644b00ca34d823d7c010ac101304140
Plamen Neshkov <plamenneshkov@ghostmail.com>
2016-01-18 01:43:41 +0200

    Implement choosing

--- Java-Data-Structures/src/KaraokeMachine.java
+++ Java-Data-Structures/src/KaraokeMachine.java
@@ -4,7 +4,9 @@ import model.SongBook;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class KaraokeMachine {
@@ -15,8 +17,9 @@ public class KaraokeMachine {
     public KaraokeMachine(SongBook songBook) {
         mSongBook = songBook;
         mReader = new BufferedReader(new InputStreamReader(System.in));
-        mMenu = new HashMap<String, String>();
+        mMenu = new HashMap<>();
         mMenu.put("add", "Add a new song to the song book");
+        mMenu.put("choose", "Choose a song to sing!");
         mMenu.put("quit", "Give up. Exit the program");
     }
 
@@ -41,6 +44,12 @@ public class KaraokeMachine {
                         Song song = promptNewSong();
                         mSongBook.addSong(song);
                         System.out.printf("%s added!%n%n", song);
+                        break;
+                    case "choose":
+                        String artist = promptArtist();
+                        Song artistSong = promptSongForArtist(artist);
+                        // TODO: Add song queue
+                        System.out.printf("You chose: %s %n", artistSong);
                         break;
                     case "quit":
                         System.out.println("Thanks for playing!");
@@ -67,4 +76,32 @@ public class KaraokeMachine {
         return new Song(artist, title, videoUrl);
     }
 
+    private String promptArtist() throws IOException {
+        System.out.println("Available artists:");
+        List<String> artists = new ArrayList<>(mSongBook.getArtists());
+        int index = promptForIndex(artists);
+        return artists.get(index);
+    }
+
+    private Song promptSongForArtist(String artist) throws IOException {
+        List<Song> songs = mSongBook.getSongsForArtist(artist);
+        List<String> songTitles = new ArrayList<>();
+        for (Song song : songs) {
+            songTitles.add(song.getTitle());
+        }
+        int index = promptForIndex(songTitles);
+        return songs.get(index);
+    }
+
+    private int promptForIndex(List<String> options) throws IOException {
+        int counter = 1;
+        for (String option : options) {
+            System.out.printf("%d.) %s%n", counter, option);
+            counter++;
+        }
+        String optionAsString = mReader.readLine();
+        int choice = Integer.parseInt(optionAsString.trim());
+        System.out.print("Your choice: ");
+        return choice - 1;
+    }
 }
--- Java-Data-Structures/src/model/SongBook.java
+++ Java-Data-Structures/src/model/SongBook.java
@@ -1,13 +1,12 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SongBook {
     private List<Song> mSongs;
 
     public SongBook() {
-        mSongs = new ArrayList<Song>();
+        mSongs = new ArrayList<>();
     }
 
     public void addSong(Song song) {
@@ -16,5 +15,31 @@ public class SongBook {
 
     public int getSongCount() {
         return mSongs.size();
+    }
+
+    // FIXME: Should be cached
+    private Map<String, List<Song>> byArtist() {
+        Map<String, List<Song>> byArtist = new HashMap<>();
+
+        for (Song song : mSongs) {
+            List<Song> artistSongs = byArtist.get(song.getArtist());
+
+            if (artistSongs == null) {
+                artistSongs = new ArrayList<>();
+                byArtist.put(song.getArtist(), artistSongs);
+            }
+
+            artistSongs.add(song);
+        }
+
+        return byArtist;
+    }
+
+    public Set<String> getArtists() {
+        return byArtist().keySet();
+    }
+
+    public List<Song> getSongsForArtist(String artistName) {
+        return byArtist().get(artistName);
     }
 }
