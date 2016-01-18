17251d3fe94648eb2dfe5e1f1cec4b18afc6d85c
Plamen Neshkov <plamen0neshkov@gmail.com>
2016-01-18 23:50:54 +0200

    Forgot to commit these...fuck

---
+++ Unit-Testing/tests/com/teamtreehouse/vending/CreditorTest.java
@@ -0,0 +1,18 @@
+package com.teamtreehouse.vending;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreditorTest {
+
+    @Test
+    public void testRefund() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void addingFundsIncrementsAvailableFunds() throws Exception {
+
+    }
+}
