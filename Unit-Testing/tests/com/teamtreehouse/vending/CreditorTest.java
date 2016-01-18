9e4fc0cb595b2d0ae7afe13a1610c6c755184f83
Plamen Neshkov <plamenneshkov@ghostmail.com>
2016-01-19 01:53:33 +0200

    Finish unit testing

--- Unit-Testing/tests/com/teamtreehouse/vending/CreditorTest.java
+++ Unit-Testing/tests/com/teamtreehouse/vending/CreditorTest.java
@@ -1,18 +1,47 @@
 package com.teamtreehouse.vending;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CreditorTest {
 
-    @Test
-    public void testRefund() throws Exception {
-        fail();
+    private Creditor creditor;
+
+    @Before
+    public void setUp() throws Exception {
+        creditor = new Creditor();
     }
 
     @Test
     public void addingFundsIncrementsAvailableFunds() throws Exception {
+        creditor.addFunds(25);
+        creditor.addFunds(25);
 
+        assertEquals(50, creditor.getAvailableFunds());
+    }
+
+    @Test
+    public void refundingReturnsAllAvailableFunds() throws Exception {
+        creditor.addFunds(10);
+
+        int refund = creditor.refund();
+
+        assertEquals(10, refund);
+    }
+
+    @Test
+    public void refundingResetsAvailableFundsToZero() {
+        creditor.addFunds(10);
+
+        creditor.refund();
+
+        assertEquals(0, creditor.getAvailableFunds());
+    }
+
+    @Test(expected = NotEnoughFundsException.class)
+    public void deductingMoreMoneyThanAvailableFundsIsNotAllowed() throws Exception {
+        creditor.deduct(1);
     }
 }
